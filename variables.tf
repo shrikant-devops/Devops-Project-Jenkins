5b95e3625ad1f6a4b4d4a005a4b42e82bcac5dce
shrikant-devops <shrikant700091@gmail.com>
2024-12-16 19:41:46 +0530

    Update and rename variables (1).tf to variables.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 variable "aws_region" {
   description = "AWS region"
-  default = "us-east-2"
+  default = "us-south-1"
 }
 
 variable "aws_type" {
@@ -10,5 +10,5 @@ variable "aws_type" {
 
 variable "aws_ami" {
   description = "AWS ami"
-  default = "ami-022661f8a4a1b91cf"
+  default = "ami-0614680123427b75e"
 }
